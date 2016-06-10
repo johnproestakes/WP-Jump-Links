1bbbcb9aba4f13bfa81733e3ed1b0c63fc39d19a
John Proestakes <jproestakes@gmail.com>
2016-06-10 09:07:56 -0400

    Reorg

--- _archive/admin-notice.php
+++
@@ -1,5 +0,0 @@
-<div class="error fade"><p>
-<strong>Starling doesnt have keys set.</strong>
-<a href="admin.php?page=starling-int-app-jjp">Fix it</a>
-</p>
-</div>
--- _archive/import_csv.php
+++
@@ -1,7 +0,0 @@
-<?php $frmAction = get_admin_url()."admin-post.php";?> 
-<div class="callout"><form method="post" enctype="multipart/form-data" action="<? echo $frmAction; ?>">
-<h2>Upload your CSV file.</h2>
-<input size='50' type='file' name='filename'>
-<input name="action" value="import_csv_submit_form" type="hidden"/>
-<?php submit_button('Upload CSV File'); ?>
-</form></div>
